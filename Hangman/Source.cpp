3cf3fed5267a5b03c2ba1dba13d87a203737f8b3
jano1338 <matejka.janko@gmail.com>
2022-05-18 06:18:21 +0200

    Add project files.

---
+++ Hangman/Source.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <cstdlib>
+#include<ctime>
+#include <string>
+using namespace std;
+
+int NUM_TRY = 4;
+int checkGuess(char, string, string&);
+void main_menu();
+string message = "Hrat!";
+
+
+int main(int argc, char* argv[])
+{
+	string name;
+	char letter;
+	string month;
+
+
+	string months[] =
+	{
+		"januar",
+		"februar",
+		"marec",
+		"april",
+		"maj",
+		"jun",
+		"jul",
+		"august",
+		"september",
+		"oktober",
+		"november",
+		"december"
+	};
+
+	srand(time(NULL));
+	int n = rand() % 12;
+	month = months[n];
+
+
+	string hide_m(month.length(), 'X');
+
+
+
+
+	while (NUM_TRY != 0)
+	{
+		main_menu();
+		cout << "\n\n\t\t\t\t" << hide_m;
+		cout << "\n\n\t\t\t\tHadaj pismenko: ";
+		cin >> letter;
+		 
+		if (checkGuess(letter, month, hide_m) == 0)
+		{
+			message = "Zle pismemnko.";
+			NUM_TRY = NUM_TRY - 1;
+		}
+		else
+		{
+			message = "Uhadol si pismenko";
+		}
+
+
+
+		if (month == hide_m)
+		{
+			message = "Gratulujem! Uhadol si.";
+			main_menu();
+			cout << "\n\t\t\t\tMesiac je : " << month << endl;
+			break;
+		}
+	}
+	if (NUM_TRY == 0)
+	{
+		message = "Prehral si.";
+		main_menu();
+		cout << "\n\t\t\t\tMesiac bol : " << month << endl;
+	}
+	cin.ignore();
+	cin.get();
+	return 0;
+}
+
+
+int checkGuess(char guess, string secretmonth, string& guessmonth)
+{
+	int i;
+	int matches = 0;
+	int len = secretmonth.length();
+	for (i = 0; i < len; i++)
+	{
+
+		if (guess == guessmonth[i])
+			return 0;
+
+		if (guess == secretmonth[i])
+		{
+			guessmonth[i] = guess;
+			matches++;
+		}
+	}
+	return matches;
+}
+
+void main_menu()
+{
+	system("color 02 ");
+	system("cls");
+	cout << "\t\t\t\t*\t*";
+
+	cout << "\t\t\t\t**\t**";
+	cout << "\t\t\t\t***\t***";
+	cout << "\t\t\t\t****\t****";
+	cout << "\t\t\t\t*****\t*****";
+	cout << "\t\t\t\t******\t******";
+	cout << "\t\t\t\t*******\t*******";
+	cout << "\t\t\t\t*******\t*******";
+	cout << "\t\t\t\t******\t******";
+	cout << "\t\t\t\t*****\t*****";
+	cout << "\t\t\t\t****\t****";
+	cout << "\t\t\t\t***\t***";
+	cout << "\t\t\t\t**\t**";
+	cout << "\t\t\t\t*\t*";
+
+	
+	cout << "\n\t\t\t\tObesenec!";
+	cout << "\n\t\tMas este " << NUM_TRY << " pokusi. Hadaj nazvi mesiacov.";
+	cout << "\n\n\t\t\t\t" + message;
+	cout << "\n\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+
+}
